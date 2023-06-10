7c3040af60a5c8aa53537812505b69da1b5cc997
Nur <furiousnur@gmail.com>
2023-06-10 14:00:57 +0600

    admin modify

--- resources/views/backend/layouts/sidebar.blade.php
+++ resources/views/backend/layouts/sidebar.blade.php
@@ -10,28 +10,28 @@
             </li>
         @endcan
         @can('location-sidebar')
-            <li><a class="app-menu__item {{ Request::segment(1) == 'location'  ? 'active' : ''}}" href="{{route('location.index')}}"><i class="app-menu__icon fa fa-history"></i><span class="app-menu__label">Locations</span></a></li>
+            <li><a class="app-menu__item {{ Request::segment(1) == 'location'  ? 'active' : ''}}" href="{{route('location.index')}}"><i class="app-menu__icon fa fa-location-arrow"></i><span class="app-menu__label">Locations</span></a></li>
         @endcan
         @can('location-price-sidebar')
-            <li><a class="app-menu__item {{ Request::segment(1) == 'location-price'  ? 'active' : ''}}" href="{{route('location-price.index')}}"><i class="app-menu__icon fa fa-history"></i><span class="app-menu__label">Location Price</span></a></li>
+            <li><a class="app-menu__item {{ Request::segment(1) == 'location-price'  ? 'active' : ''}}" href="{{route('location-price.index')}}"><i class="app-menu__icon fa fa-money"></i><span class="app-menu__label">Location Price</span></a></li>
         @endcan
         @can('booking-sidebar')
-            <li><a class="app-menu__item {{ Request::segment(1) == 'booking'  ? 'active' : ''}}" href="{{route('booking.index')}}"><i class="app-menu__icon fa fa-history"></i><span class="app-menu__label">Booking List</span></a></li>
+            <li><a class="app-menu__item {{ Request::segment(1) == 'booking'  ? 'active' : ''}}" href="{{route('booking.index')}}"><i class="app-menu__icon fa fa-save"></i><span class="app-menu__label">Booking List</span></a></li>
         @endcan
         @can('payment-sidebar')
-            <li><a class="app-menu__item {{ Request::segment(1) == 'payment-list'  ? 'active' : ''}}" href="{{route('payment.list')}}"><i class="app-menu__icon fa fa-history"></i><span class="app-menu__label">Payment List</span></a></li>
+            <li><a class="app-menu__item {{ Request::segment(1) == 'payment-list'  ? 'active' : ''}}" href="{{route('payment.list')}}"><i class="app-menu__icon fa fa-paypal"></i><span class="app-menu__label">Payment List</span></a></li>
         @endcan
         @can('passenger-sidebar')
-            <li><a class="app-menu__item {{ Request::segment(1) == 'passenger'  ? 'active' : ''}}" href="{{route('passenger.list')}}"><i class="app-menu__icon fa fa-history"></i><span class="app-menu__label">Passengers</span></a></li>
+            <li><a class="app-menu__item {{ Request::segment(1) == 'passenger'  ? 'active' : ''}}" href="{{route('passenger.list')}}"><i class="app-menu__icon fa fa-users"></i><span class="app-menu__label">Passengers</span></a></li>
         @endcan
         @can('driver-sidebar')
-            <li><a class="app-menu__item {{ Request::segment(1) == 'driver-list'  ? 'active' : ''}}" href="{{route('driver.list')}}"><i class="app-menu__icon fa fa-history"></i><span class="app-menu__label">Drivers</span></a></li>
+            <li><a class="app-menu__item {{ Request::segment(1) == 'driver-list'  ? 'active' : ''}}" href="{{route('driver.list')}}"><i class="app-menu__icon fa fa-users"></i><span class="app-menu__label">Drivers</span></a></li>
         @endcan
         @can('user-sidebar')
-            <li><a class="app-menu__item {{ Request::segment(1) == 'users'  ? 'active' : ''}}" href="{{route('users.index')}}"><i class="app-menu__icon fa fa-history"></i><span class="app-menu__label">All Users</span></a></li>
+            <li><a class="app-menu__item {{ Request::segment(1) == 'users'  ? 'active' : ''}}" href="{{route('users.index')}}"><i class="app-menu__icon fa fa-users"></i><span class="app-menu__label">All Users</span></a></li>
         @endcan
         @can('role-sidebar')
-            <li><a class="app-menu__item {{ Request::segment(1) == 'roles'  ? 'active' : ''}}" href="{{route('roles.index')}}"><i class="app-menu__icon fa fa-history"></i><span class="app-menu__label">Roles</span></a></li>
+            <li><a class="app-menu__item {{ Request::segment(1) == 'roles'  ? 'active' : ''}}" href="{{route('roles.index')}}"><i class="app-menu__icon fa fa-assistive-listening-systems"></i><span class="app-menu__label">Roles</span></a></li>
         @endcan
 
     </ul>
--- resources/views/backend/pages/dashboard.blade.php
+++ resources/views/backend/pages/dashboard.blade.php
@@ -11,7 +11,7 @@
             </ul>
         </div>
         <div class="row">
-            <div class="col-md-6 col-lg-3">
+            <div class="col-md-6 col-lg-6">
                 <div class="widget-small primary coloured-icon"><i class="icon fa fa-users fa-3x"></i>
                     <div class="info">
                         <h4>Total User</h4>
@@ -19,7 +19,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-6 col-lg-3">
+            <div class="col-md-6 col-lg-6">
                 <div class="widget-small info coloured-icon"><i class="icon fa fa-users fa-3x"></i>
                     <div class="info">
                         <h4>Total Passenger</h4>
@@ -27,7 +27,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-6 col-lg-3">
+            <div class="col-md-6 col-lg-6">
                 <div class="widget-small warning coloured-icon"><i class="icon fa fa-users fa-3x"></i>
                     <div class="info">
                         <h4>Total Driver</h4>
@@ -35,29 +35,11 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-6 col-lg-3">
+            <div class="col-md-6 col-lg-6">
                 <div class="widget-small danger coloured-icon"><i class="icon fa fa-location-arrow fa-3x"></i>
                     <div class="info">
                         <h4>Total Location</h4>
                         <p><b>{{$totalLocation}}</b></p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-6">
-                <div class="tile">
-                    <h3 class="tile-title">Customers</h3>
-                    <div class="embed-responsive embed-responsive-16by9">
-                        <canvas class="embed-responsive-item" id="lineChartDemo"></canvas>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class="tile">
-                    <h3 class="tile-title">Support Requests</h3>
-                    <div class="embed-responsive embed-responsive-16by9">
-                        <canvas class="embed-responsive-item" id="pieChartDemo"></canvas>
                     </div>
                 </div>
             </div>
